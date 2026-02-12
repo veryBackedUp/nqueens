4564d7bfa5f1162147a3e78625cb441d62de393a
Christian Aguilera <111899614+cfaguilera@users.noreply.github.com>
2026-02-11 17:53:39 -0700

    Revert "All solutions"

--- solver.c
+++ solver.c
@@ -4,7 +4,7 @@
 bool solve_nqueens_util(int board[], int col, int n) {
     if (col >= n) {
         print_board(board, n); // Print the current solution
-        return false; 
+        return true; 
     }
 
     for (int i = 0; i < n; i++) {
